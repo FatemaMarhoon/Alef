5c3cac27b36807cd89a1b26b9313b72dcfb54d42
Fatema Marhoon <118673837+FatemaMarhoon@users.noreply.github.com>
2023-11-26 13:42:45 +0300

    Revert "Revert "Fatema class assignment""

--- Client/app/class/view2/page.tsx
+++ Client/app/class/view2/page.tsx
@@ -1,9 +1,10 @@
 // Import necessary modules and components
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import CreateForm from '@/components/Forms/createClassForm';
 import { NextPage } from 'next';
 import { useSearchParams } from 'next/navigation';
+import { Class } from '@/types/class';
 
 interface CreatePageProps {
     numClasses: number;
@@ -13,13 +14,22 @@ interface CreatePageProps {
 // Use NextPage type and pass the props directly
 const createPage: NextPage<CreatePageProps> = ({ numClasses, grade }) => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [createdClasses, setCreatedClasses] = useState<Class[]>([]);
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const searchParams = useSearchParams();
     console.log('Props in CreatePage:', Number(searchParams.get(numClasses)), searchParams.get(grade)); // Check if values are printed here
 
+    const handleCreateClasses = (newClasses: Class[]) => {
+        // Handle the created classes or class IDs here
+        console.log('Created classes:', newClasses);
+        setCreatedClasses(newClasses); // Update state with created classes
+    };
+
     return (
         <>
-            {/* Pass the parameters to the CreateForm component */}
-            <CreateForm numClasses={numClasses} grade={grade} />
+            {/* Pass the parameters and createdClasses to the CreateForm component */}
+            <CreateForm numClasses={numClasses} grade={grade} createdClasses={createdClasses} />
         </>
     );
 };
