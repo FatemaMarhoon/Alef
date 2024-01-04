60ca3b55953f2dcecdffa071ca03cbe836aab23f
KawtharAlakri <202003447@student.polytechnic.bh>
2024-01-04 03:01:37 +0300

    replace all local hosts

--- Client/services/userService.ts
+++ Client/services/userService.ts
@@ -3,8 +3,8 @@ import { User } from '@/types/user'
 import { currentPreschool, currentToken } from './authService';
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
-// const BASE_URL = 'https://server-bckggkpqeq-uc.a.run.app/users'; // server URL
-const BASE_URL = 'http://localhost:3001/users'; // server URL
+const BASE_URL = 'https://server-bckggkpqeq-uc.a.run.app/users'; // server URL
+// const BASE_URL = 'http://localhost:3001/users'; // server URL
 
 export async function getUsers(): Promise<User[]> {
   //retrieve token and preschool from current user
